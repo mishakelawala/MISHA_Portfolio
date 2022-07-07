1f3d5110829249085954ab549ac4bc787e6265b2
Misha <weingenioustechnocrats@gmail.com>
2022-07-07 17:43:14 +0530

    first commit

---
+++ src/components/About/index.js
@@ -0,0 +1,84 @@
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact
+} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+
+const About = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 3000)
+  }, [])
+
+  return (
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            Design and implement user interface components for JavaScript-based web and mobile applications using React ecosystem.
+          </p>
+          <p>
+            Work as a part of a small team that includes other react native developers, designers, QA experts, and managers
+            Work with native modules as and when required
+          </p>
+          <p align="LEFT">
+            Use Native APIs for tight integrations with both platforms – Android and iOS
+            Create front-end modules with maximum code reusability and efficiency
+          </p>
+          <p>
+            Implement clean, smooth animations to provide an excellent user interface
+            Work with third-party dependencies and APIs
+          </p>
+          <p>
+            Work with Redux architecture to improve performance of the websites/mobile apps
+            Coordinate with the cross-functional teams to build apps in stipulated time/budget.
+          </p>
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default About
