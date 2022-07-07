387b162a78ca5bf3a4cfa6252b086ee220e8f1ed
Misha <weingenioustechnocrats@gmail.com>
2022-07-07 18:13:43 +0530

    1 commit

--- src/index.js
+++ src/index.js
@@ -8,10 +8,7 @@ import {BrowserRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basemname={`/${process.env.PUBLIC_URL}`}>
       <App />
-    </Router>
-    ,
   </React.StrictMode>,
   document.getElementById('root')
 )
